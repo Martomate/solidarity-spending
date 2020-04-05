a16521741fbb36390d61c8962b250195ccebe816
Martin Jakobsson <martin.jakobsson@weiq.tech>
2020-04-05 18:16:43 +0200

    Added some css

--- app.py
+++ app.py
@@ -5,11 +5,26 @@ app = Flask(__name__)
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return '<h1>Welcome to Solidarity Spendning!</h1><p>This is a demo page, and not the final product.</p><p>There is a map <a href="/map">here</a> and a Facebook test <a href="/fb_test">here</a></p>'
+    return '''
+    <html>
+      <head>
+        <title>Solidarity Spending</title>
+        <link rel="stylesheet" href="/css/main.css" />
+      </head>
+      <body>
+        <h1>Welcome to Solidarity Spendning!</h1>
+        <p>This is a demo page, and not the final product.</p><p>There is a map <a href="/map">here</a> and a Facebook test <a href="/fb_test">here</a></p>
+      </body>
+    </html>
+    '''
 
 @app.route('/js/<path:path>')
 def serveJavascript(path):
     return send_from_directory('js', path)
+
+@app.route('/css/<path:path>')
+def serveCSS(path):
+    return send_from_directory('css', path)
 
 @app.route('/map/')
 def map():
@@ -24,7 +39,8 @@ def privacy_policy():
     return '''
     <html>
       <head>
-        <title>Privacy Policy</title>
+        <title>Privacy Policy - Solidarity Spending</title>
+        <link rel="stylesheet" href="/css/main.css" />
       </head>
       <body>
         <h1>Privacy Policy for Solidarity Spending</h1>
